7303125f865e5a34581806d08a9bbd9be564626d
Nabiha Khan <nabiha.khan@me.com>
2021-08-25 21:30:57 -0700

    cleaning schema and trying orm method?? lets see!

--- db/schema.sql
+++ db/schema.sql
@@ -4,30 +4,30 @@ CREATE DATABASE s2genomics_db;
 USE s2genomics_db;
 
 -- create a table for department information
-CREATE TABLE department (
-    dept_id INT NOT NULL AUTO_INCREMENT,
-    dept_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (dept_id)
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 -- create a table for employees
 CREATE TABLE employees (
-    emp_id INT(10) NOT NULL AUTO_INCREMENT,
+    id INT(10) NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    emp_role_id INT(10) NOT NULL,
+    role_id INT(10) NOT NULL,
     manager_id INT(10),
-    PRIMARY KEY (emp_id)
+    PRIMARY KEY (id)
 );
 
 -- create a table for employee roles
 
-CREATE TABLE s2_role (
-    role_id INT(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE roles (
+    id INT(10) NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DEC(10,2) NOT NULL,
-    emp_role_id INT,
-    PRIMARY KEY (role_id)
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT(10) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 -- used help from online repos to tabulate this particular repo as a guide
